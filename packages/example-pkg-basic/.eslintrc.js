41782dd4775c0aeb95669d2f6b7628405978cbf0
Apollo Tang <tang.apollo@gmail.com>
2021-08-25 02:43:26 -0400

    chore: work in cli but not in ide

--- packages/example-pkg-basic/.eslintrc.js
+++ packages/example-pkg-basic/.eslintrc.js
@@ -1,13 +1,22 @@
+/* xxxxeslint-disable */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 module.exports = {
   root: true,
   extends: [
-    '@rush-monorepo/eslint-config/typescript/node',
+    '@rush-monorepo/eslint-config/myconfig',
+    // '@rush-monorepo/eslint-config/typescript/react',
     '@rush-monorepo/eslint-config/prettier',
   ],
+
   parserOptions: {
-    project: 'tsconfig.json',
+    project: './tsconfig.json',
+    createDefaultProgram: true,
     tsconfigRootDir: __dirname,
     sourceType: 'module',
     ecmaVersion: 2015,
   },
+  // rules: {
+  //   'no-console': 'warn',
+  // },
 };
